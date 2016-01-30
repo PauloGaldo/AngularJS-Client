8e5bb1166a14aa0829d0879b57eeb310fa0f8cf1
Paulo Galdo <hades_54st@hotmail.com>
2016-01-30 05:41:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 var express = require('express'),
 app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public_html'));
 app.get('/', function(req, res) {
     res.redirect('index.html')
 });
