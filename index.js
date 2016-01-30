c7e98321950f16ade2df53ff7e54577f9680921d
Paulo Galdo <hades_54st@hotmail.com>
2016-01-30 05:40:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@
 var express = require('express'),
 app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
-    res.sendfile('index.html', {root: __dirname })
+    res.redirect('index.html')
 });
 var server = app.listen(process.env.PORT || 80);
 
