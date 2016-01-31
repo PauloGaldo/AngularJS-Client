1a1995d15580865475b880700f8e8bc629539ca0
Paulo Galdo <hades_54st@hotmail.com>
2016-01-30 23:15:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ var express = require('express'),
 app = express();
 
 app.use(express.static(__dirname + '/public_html'));
-app.get('/', function(req, res) {
-    res.redirect('https://')
-});
+
+
+function requireHTTPS(req, res, next) {
+    if (!req.secure) {
+        //FYI this should work for local development as well
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+app.use(requireHTTPS);
 
 var server = app.listen(process.env.PORT || 443);
 
